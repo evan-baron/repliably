a279c93fde20c6f6a4062da7f18032b048f59dbd
evan-baron <egbaron@gmail.com>
2025-12-29 13:19:40 -0700

    extracted contactstable to reusable component

---
+++ src/app/components/contacts/ContactsTable.tsx
@@ -0,0 +1,147 @@
+'use client';
+
+// Library imports
+import { useState } from 'react';
+
+// Hooks imports
+
+// Styles imports
+import styles from './contactsTable.module.scss';
+
+// MUI imports
+import { SwapVert } from '@mui/icons-material';
+
+// Components imports
+
+// Context imports
+import { useAppContext } from '@/app/context/AppContext';
+
+// Types imports
+import { ContactFromDB } from '@/types/contactTypes';
+
+const ContactsTable = ({
+	contacts,
+	onRowClick,
+	columns,
+}: {
+	contacts: ContactFromDB[];
+	onRowClick: (contact: ContactFromDB) => void;
+	columns: {
+		associatedRole: boolean;
+		importance: boolean;
+		lastActivity: boolean;
+		linkedIn: boolean;
+		phone: boolean;
+		replied: boolean;
+	};
+}) => {
+	console.log(contacts);
+	const { setSelectedContact } = useAppContext();
+	type SortableContactColumn = 'firstName' | 'lastName' | 'company';
+
+	const [sortBy, setSortBy] = useState<SortableContactColumn | null>(null);
+	const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+
+	const handleSort = (column: SortableContactColumn) => {
+		if (sortBy === column) {
+			setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+		} else {
+			setSortBy(column);
+			setSortOrder('asc');
+		}
+	};
+
+	const handleClick = (contact: ContactFromDB) => {
+		setSelectedContact(contact);
+		onRowClick(contact);
+	};
+
+	const sortedContacts = [...contacts].sort((a, b) => {
+		if (!sortBy) return 0;
+		const valA = (a[sortBy] || '').toString().toLowerCase();
+		const valB = (b[sortBy] || '').toString().toLowerCase();
+		if (valA < valB) return sortOrder === 'asc' ? -1 : 1;
+		if (valA > valB) return sortOrder === 'asc' ? 1 : -1;
+		return 0;
+	});
+
+	return (
+		<table className={styles['contacts-table']}>
+			<thead>
+				<tr>
+					<th className={styles.sm} onClick={() => handleSort('firstName')}>
+						<span className={styles.sort}>
+							First
+							<SwapVert fontSize='small' />
+						</span>
+					</th>
+					<th className={styles.sm} onClick={() => handleSort('lastName')}>
+						<span className={styles.sort}>
+							Last
+							<SwapVert fontSize='small' />
+						</span>
+					</th>
+					<th className={styles.lrg} onClick={() => handleSort('company')}>
+						<span className={styles.sort}>
+							Company
+							<SwapVert fontSize='small' />
+						</span>
+					</th>
+					<th className={styles.md}>Title</th>
+					{columns.importance && <th className={styles.sm}>Priority</th>}
+					{columns.phone && <th className={styles.md}>Phone</th>}
+					<th className={styles.lrg}>Email</th>
+					{columns.linkedIn && <th className={styles.md}>LinkedIn</th>}
+					{columns.lastActivity && (
+						<th className={styles.sm}>Last Contacted</th>
+					)}
+					{columns.replied && <th className={styles.sm}>Replied</th>}
+					{columns.associatedRole && (
+						<th className={styles.lrg}>Role Applied</th>
+					)}
+				</tr>
+			</thead>
+			<tbody>
+				{sortedContacts.map((contact) => (
+					<tr key={contact.email} onClick={() => handleClick(contact)}>
+						<td className={styles.sm}>{contact.firstName}</td>
+						<td className={styles.sm}>{contact.lastName}</td>
+						<td className={styles.md}>{contact.company}</td>
+						<td className={styles.md}>{contact.title}</td>
+						{columns.importance && (
+							<td className={`${styles.sm} ${styles.right}`}>
+								{contact.importance}
+							</td>
+						)}
+						{columns.phone && (
+							<td className={`${styles.sm} ${styles.right}`}>
+								{contact.phone}
+							</td>
+						)}
+						<th className={styles.lrg}>{contact.email}</th>
+						{columns.linkedIn && (
+							<td className={styles.md}>{contact.linkedIn}</td>
+						)}
+						{columns.lastActivity && (
+							<td className={`${styles.sm} ${styles.right}`}>
+								{contact.lastActivity
+									? new Date(contact.lastActivity).toLocaleDateString()
+									: ''}
+							</td>
+						)}
+						{columns.replied && (
+							<td className={`${styles.sm} ${styles.right}`}>
+								{contact.replied ? 'Yes' : 'No'}
+							</td>
+						)}
+						{columns.associatedRole && (
+							<td className={styles.lrg}>{contact.associatedRole}</td>
+						)}
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
+};
+
+export default ContactsTable;
--- src/app/components/modal/modalTypes/contacts/SearchContactsModal.tsx
+++ src/app/components/modal/modalTypes/contacts/SearchContactsModal.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 // Library imports
-import { useState } from 'react';
 import { useContactsGetAll } from '@/hooks/useContact';
 
 // Hooks imports
@@ -10,50 +9,26 @@ import { useContactsGetAll } from '@/hooks/useContact';
 import styles from './searchContactsModal.module.scss';
 
 // MUI imports
-import { SwapVert, Refresh } from '@mui/icons-material';
+import { Refresh } from '@mui/icons-material';
 
 // Components imports
 import SearchBar from '@/app/components/ui/searchBar/SearchBar';
+import ContactsTable from '@/app/components/contacts/ContactsTable';
 
 // Context imports
 import { useAppContext } from '@/app/context/AppContext';
 
 // Types imports
-import { ContactFromDB } from '@/types/contactTypes';
 
 const SearchContactsModal = () => {
-	const { setSelectedContact, setModalType } = useAppContext();
+	const { setModalType } = useAppContext();
 	const { data: contactsData, error, loading, refetch } = useContactsGetAll();
-
-	type SortableContactColumn = 'firstName' | 'lastName' | 'company';
-
-	const [sortBy, setSortBy] = useState<SortableContactColumn | null>(null);
-	const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
 
 	const contacts = contactsData?.contacts || [];
 
-	const handleSort = (column: SortableContactColumn) => {
-		if (sortBy === column) {
-			setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-		} else {
-			setSortBy(column);
-			setSortOrder('asc');
-		}
-	};
-
-	const handleClick = (contact: ContactFromDB) => {
-		setSelectedContact(contact);
+	const onRowClick = () => {
 		setModalType(null);
 	};
-
-	const sortedContacts = [...contacts].sort((a, b) => {
-		if (!sortBy) return 0;
-		const valA = (a[sortBy] || '').toString().toLowerCase();
-		const valB = (b[sortBy] || '').toString().toLowerCase();
-		if (valA < valB) return sortOrder === 'asc' ? -1 : 1;
-		if (valA > valB) return sortOrder === 'asc' ? 1 : -1;
-		return 0;
-	});
 
 	return (
 		<div className={styles['searchcontactsmodal-wrapper']}>
@@ -68,49 +43,18 @@ const SearchContactsModal = () => {
 				</button>
 			</div>
 			{error && <p style={{ color: 'red' }}>Error: {error}</p>}
-			<table className={styles['contacts-table']}>
-				<thead>
-					<tr>
-						<th className={styles.sm} onClick={() => handleSort('firstName')}>
-							<span className={styles.sort}>
-								First
-								<SwapVert fontSize='small' />
-							</span>
-						</th>
-						<th className={styles.sm} onClick={() => handleSort('lastName')}>
-							<span className={styles.sort}>
-								Last
-								<SwapVert fontSize='small' />
-							</span>
-						</th>
-						<th className={styles.lrg} onClick={() => handleSort('company')}>
-							<span className={styles.sort}>
-								Company
-								<SwapVert fontSize='small' />
-							</span>
-						</th>
-						<th className={styles.md}>Title</th>
-						<th className={styles.lrg}>Email</th>
-						<th className={styles.sm}>Last Contacted</th>
-					</tr>
-				</thead>
-				<tbody>
-					{sortedContacts.map((contact) => (
-						<tr key={contact.email} onClick={() => handleClick(contact)}>
-							<td className={styles.sm}>{contact.firstName}</td>
-							<td className={styles.sm}>{contact.lastName}</td>
-							<td className={styles.md}>{contact.company}</td>
-							<td className={styles.md}>{contact.title}</td>
-							<td className={styles.lrg}>{contact.email}</td>
-							<td className={styles.sm}>
-								{contact.lastActivity
-									? new Date(contact.lastActivity).toLocaleDateString()
-									: ''}
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<ContactsTable
+				contacts={contacts}
+				onRowClick={onRowClick}
+				columns={{
+					associatedRole: false,
+					importance: false,
+					lastActivity: true,
+					linkedIn: false,
+					phone: false,
+					replied: false,
+				}}
+			/>
 		</div>
 	);
 };
