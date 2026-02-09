e09838af83952826683b18716647e2d10ceacb2b
evan-baron <egbaron@gmail.com>
2026-02-09 12:47:16 -0700

    sequence default settings complete

--- src/app/components/forms/sequenceDefaultSettings/SequenceDefaultsForm.tsx
+++ src/app/components/forms/sequenceDefaultSettings/SequenceDefaultsForm.tsx
@@ -3,7 +3,9 @@
 // Library imports
 import { useState, useEffect } from 'react';
 import { useForm, SubmitHandler, FieldErrors } from 'react-hook-form';
-import Link from 'next/link';
+
+// Hooks imports
+import { useUserAccountSettingsUpdate } from '@/hooks/useUserSettings';
 
 // Styles imports
 import styles from '../settingsForm.module.scss';
@@ -13,7 +15,6 @@ import { UserToClientFromDB } from '@/types/userTypes';
 
 // Context imports
 import { useAppContext } from '@/app/context/AppContext';
-import { useSettingsContext } from '@/app/context/SettingsContext';
 
 const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 	const {
@@ -25,31 +26,83 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 		setLoading,
 		setLoadingMessage,
 	} = useAppContext();
-	const { isSaving, setIsSaving, setActiveTab } = useSettingsContext();
+	const { mutateAsync: updateUser, isPending: updatingUser } =
+		useUserAccountSettingsUpdate();
 
 	interface SequenceDefaultsFormData {
-		autoSend: boolean;
-		sequenceDuration: number;
-		referencePreviousEmail: boolean;
-		alterSubjectLine: boolean;
-		autoSendDelay: number;
-		sequenceType: number;
+		defaultRequireApproval: boolean;
+		defaultSequenceDuration: number;
+		defaultReferencePrevious: boolean;
+		defaultAlterSubjectLine: boolean;
+		defaultSendDelay: number;
+		defaultSequenceType: number;
 	}
+
+	const initialValues = {
+		defaultRequireApproval: user.defaultRequireApproval ?? false,
+		defaultSequenceDuration: user.defaultSequenceDuration ?? 60,
+		defaultReferencePrevious: user.defaultReferencePrevious ?? true,
+		defaultAlterSubjectLine: user.defaultAlterSubjectLine ?? false,
+		defaultSendDelay: user.defaultSendDelay ?? 0,
+		defaultSequenceType: user.defaultSequenceType ?? 3,
+	};
 
 	const { register, watch, handleSubmit, reset, setValue } =
 		useForm<SequenceDefaultsFormData>({
-			defaultValues: {
-				autoSend: false,
-				sequenceDuration: 60,
-				referencePreviousEmail: true,
-				alterSubjectLine: false,
-				autoSendDelay: 0,
-				sequenceType: 3,
-			},
+			defaultValues: initialValues,
 		});
 
-	const autoSendEnabled = watch('autoSend');
-	const selectedSequenceType = watch('sequenceType');
+	// Watch all form fields
+	const formValues = watch();
+
+	// Check if form has changed
+	const [hasChanged, setHasChanged] = useState(false);
+
+	const defaultRequireApproval = watch('defaultRequireApproval');
+	const selectedDefaultSequenceType = watch('defaultSequenceType');
+
+	useEffect(() => {
+		const isChanged =
+			formValues.defaultRequireApproval !==
+				initialValues.defaultRequireApproval ||
+			formValues.defaultSequenceDuration !==
+				initialValues.defaultSequenceDuration ||
+			formValues.defaultReferencePrevious !==
+				initialValues.defaultReferencePrevious ||
+			formValues.defaultAlterSubjectLine !==
+				initialValues.defaultAlterSubjectLine ||
+			formValues.defaultSendDelay !== initialValues.defaultSendDelay ||
+			Number(formValues.defaultSequenceType) !==
+				Number(initialValues.defaultSequenceType);
+
+		setHasChanged(isChanged);
+	}, [formValues, initialValues]);
+
+	const onSubmit: SubmitHandler<SequenceDefaultsFormData> = async (data) => {
+		// convert defaultSequenceDuration, defaultSendDelay and defaultSequenceType to number before sending to API
+		const payload = {
+			...data,
+			defaultSequenceDuration: Number(data.defaultSequenceDuration),
+			defaultSendDelay: Number(data.defaultSendDelay),
+			defaultSequenceType: Number(data.defaultSequenceType),
+		};
+
+		try {
+			setLoading(true);
+			setLoadingMessage('Saving');
+			await updateUser({ ...payload });
+
+			// Delaying by a small amount to ensure the user sees the loading state
+			setTimeout(() => {
+				setLoading(false);
+				setLoadingMessage(null);
+			}, 800);
+		} catch (error) {
+			// Error handling is done in the hook
+			setLoading(false);
+			setLoadingMessage(null);
+		}
+	};
 
 	const withAutoSendDeadlineOptions = [
 		{ label: '1 day', value: 1 },
@@ -62,7 +115,7 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 	];
 
 	const approvalDeadlineOptions =
-		autoSendEnabled ?
+		defaultRequireApproval ?
 			withAutoSendDeadlineOptions
 		:	withoutAutoSendDeadlineOptions;
 
@@ -74,7 +127,7 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 		{ label: 'Indefinitely', value: 0 },
 	];
 
-	const sequenceTypeOptions = [
+	const defaultSequenceTypeOptions = [
 		{
 			label: '3-day Sequence',
 			description: 'Wait 3 days → Repeat',
@@ -103,25 +156,29 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 	];
 
 	const systemDefaults = {
-		autoSend: true,
-		sequenceDuration: 60,
-		referencePreviousEmail: true,
-		alterSubjectLine: false,
-		autoSendDelay: 0,
-		sequenceType: 3,
+		defaultRequireApproval: true,
+		defaultSequenceDuration: 60,
+		defaultReferencePrevious: true,
+		defaultAlterSubjectLine: false,
+		defaultSendDelay: 0,
+		defaultSequenceType: 3,
 	};
 
 	return (
 		<div className={styles['settings-form-wrapper']}>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 				<h3 className={styles['section-title']}>Automation</h3>
 				<section className={styles.section}>
 					<div
 						className={`${styles['input-group']} ${styles['checkbox-group']}`}
 					>
-						<input type='checkbox' id='autoSend' {...register('autoSend')} />
+						<input
+							type='checkbox'
+							id='defaultRequireApproval'
+							{...register('defaultRequireApproval')}
+						/>
 						<div className={styles.input}>
-							<label htmlFor='autoSend'>
+							<label htmlFor='defaultRequireApproval'>
 								<span>Require approval for auto-generated messages</span>
 							</label>
 							<small>
@@ -131,9 +188,14 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 					</div>
 
 					<div className={styles['input-group']}>
-						<label htmlFor='sequenceDuration'>Sequence duration (days)</label>
+						<label htmlFor='defaultSequenceDuration'>
+							Sequence duration (days)
+						</label>
 						<div className={styles.input}>
-							<select id='sequenceDuration' {...register('sequenceDuration')}>
+							<select
+								id='defaultSequenceDuration'
+								{...register('defaultSequenceDuration')}
+							>
 								{sequenceDurationOptions.map((option) => (
 									<option key={option.value} value={option.value}>
 										{option.label}
@@ -154,11 +216,11 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 					>
 						<input
 							type='checkbox'
-							id='referencePreviousEmail'
-							{...register('referencePreviousEmail')}
+							id='defaultReferencePrevious'
+							{...register('defaultReferencePrevious')}
 						/>
 						<div className={styles.input}>
-							<label htmlFor='referencePreviousEmail'>
+							<label htmlFor='defaultReferencePrevious'>
 								<span>Reference previous emails in follow-ups</span>
 							</label>
 							<small>
@@ -173,11 +235,11 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 					>
 						<input
 							type='checkbox'
-							id='alterSubjectLine'
-							{...register('alterSubjectLine')}
+							id='defaultAlterSubjectLine'
+							{...register('defaultAlterSubjectLine')}
 						/>
 						<div className={styles.input}>
-							<label htmlFor='alterSubjectLine'>
+							<label htmlFor='defaultAlterSubjectLine'>
 								<span>Allow AI to alter subject lines</span>
 							</label>
 							<small>
@@ -191,12 +253,12 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 				<h3 className={styles['section-title']}>Approval Workflow</h3>
 				<section className={styles.section}>
 					<div className={styles['input-group']}>
-						<label htmlFor='autoSendDelay'>Approval deadline (days)</label>
+						<label htmlFor='approvalDeadline'>Approval deadline (days)</label>
 						<div className={styles.input}>
 							<select
-								id='autoSendDelay'
-								{...register('autoSendDelay')}
-								disabled={!autoSendEnabled}
+								id='defaultSendDelay'
+								{...register('defaultSendDelay')}
+								disabled={!defaultRequireApproval}
 							>
 								{approvalDeadlineOptions.map((option) => (
 									<option key={option.value} value={option.value}>
@@ -224,21 +286,21 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 				</div>
 				<section className={styles.section}>
 					<div className={styles['tile-grid']}>
-						{sequenceTypeOptions.map((option, index) => (
+						{defaultSequenceTypeOptions.map((option, index) => (
 							<div
 								key={index}
-								className={`${styles['tile-group']} ${Number(selectedSequenceType) === option.value ? styles.selected : ''}`}
+								className={`${styles['tile-group']} ${Number(selectedDefaultSequenceType) === option.value ? styles.selected : ''}`}
 							>
-								<label htmlFor={`sequenceType-${option.value}`}>
+								<label htmlFor={`defaultSequenceType-${option.value}`}>
 									<span>{option.label}</span>
 									<small>{option.description}</small>
+									<input
+										type='radio'
+										id={`defaultSequenceType-${option.value}`}
+										value={option.value}
+										{...register('defaultSequenceType')}
+									/>
 								</label>
-								<input
-									type='radio'
-									id={`sequenceType-${option.value}`}
-									value={option.value}
-									{...register('sequenceType')}
-								/>
 							</div>
 						))}
 					</div>
@@ -248,9 +310,9 @@ const SequenceDefaultsForm = ({ user }: { user: UserToClientFromDB }) => {
 					<button
 						className={'button save-changes'}
 						type='submit'
-						disabled={isSaving}
+						disabled={!hasChanged || updatingUser}
 					>
-						{isSaving ? 'Saving...' : 'Save Changes'}
+						{updatingUser ? 'Saving...' : 'Save Changes'}
 					</button>
 				</div>
 			</form>
