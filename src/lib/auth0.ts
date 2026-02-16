14b802535f6a4fd68fccddf43afebe322a32a1a9
evan-baron <egbaron@gmail.com>
2026-02-15 17:59:41 -0700

    adjusted auth0.ts again to accomodate logging out

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,5 +1,3 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-export const auth0 = new Auth0Client({
-	appBaseUrl: process.env.NEXT_PUBLIC_APP_URL,
-});
+export const auth0 = new Auth0Client();
